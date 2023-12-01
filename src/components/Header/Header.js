f47132a4eb8b4180564926bb53e4cfabf7f27921
quangtri <quangtri3032001@gmail.com>
2023-12-01 14:12:50 +0700

    update responsive header + product

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiMiniShoppingCart } from "react-icons/hi2";
+import { IoMdMenu } from "react-icons/io";
 
 export default function Header() {
-    return (
-        <header className="sticky top-0 w-full z-10 shadow-lg">
-            <nav className="bg-white px-10 py-3 flex flex-row items-center">
-                <a href="/" className="text-primary text-3xl basis-1/4 font-bold">Empalic</a>
-                <div className="basis-1/2 text-xl ">
-                    <ul className="flex flex-row">
-                        <li className="mx-5">
-                            <a href="/about" className="text-black hover:text-primary">Giới thiệu</a>
-                        </li>
-                        <li  className="mx-5">
-                            <a href="/service" className="text-black hover:text-primary">Dịch vụ</a>
-                        </li>
-                        <li  className="mx-5">
-                            <a href="/product" className="text-black hover:text-primary">Sản phẩm</a>
-                        </li>
-                        <li  className="mx-5">
-                            <a href="/contact" className="text-black hover:text-primary">Liên hệ</a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="basis-1/4 flex flex-row items-center justify-end">
-                    <button className="flex flex-row text-primary mx-10 text-xl border-primary border-2 rounded-xl p-2">
-                        <a href="/cart" className="flex flex-row">
-                            <HiMiniShoppingCart size={30} className="mr-3"/>
-                            Giỏ hàng
-                        </a>
-                    </button>
-                    <button className="bg-teal-500 text-white rounded-3xl p-3"><a href="/login">Đăng nhập</a></button>
-                </div>
-            </nav>
-        </header>
-    );
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="sticky top-0 w-full z-10 shadow-lg">
+      <nav className="bg-white px-5 md:px-10 py-3 flex flex-row items-center">
+        <button
+          className="text-primary mx-3 md:hidden"
+          onClick={toggleMenu}
+        >
+          <IoMdMenu size={30} />
+        </button>
+        <a href="/" className="text-primary text-3xl md:flex-1 text-center font-bold">
+          Empalic
+        </a>
+        <div className="md:flex-1 text-xl hidden md:flex justify-center">
+          <ul className="flex flex-row">
+            <li className="mx-3 md:mx-5">
+              <a href="/about" className="text-black hover:text-primary">
+                Giới thiệu
+              </a>
+            </li>
+            <li className="mx-3 md:mx-5">
+              <a href="/service" className="text-black hover:text-primary">
+                Dịch vụ
+              </a>
+            </li>
+            <li className="mx-3 md:mx-5">
+              <a href="/product" className="text-black hover:text-primary">
+                Sản phẩm
+              </a>
+            </li>
+            <li className="mx-3 md:mx-5">
+              <a href="/contact" className="text-black hover:text-primary">
+                Liên hệ
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="md:flex-1 flex flex-row items-center justify-end">
+          <button className="flex flex-row text-primary mx-3 md:mx-10 text-xl border-primary border-2 rounded-xl p-2 hidden md:flex">
+            <a href="/cart" className="flex flex-row">
+              <HiMiniShoppingCart size={30} className="mr-3" />
+              Giỏ hàng
+            </a>
+          </button>
+          <button className="bg-teal-500 text-white rounded-3xl p-3 hidden md:block">
+            <a href="/login">Đăng nhập</a>
+          </button>
+        </div>
+        {isMenuOpen && (
+          <div className="md:hidden absolute top-full left-0 bg-white w-full py-2">
+            <ul className="flex flex-col items-center">
+              <li className="my-2">
+                <a href="/about" className="text-black hover:text-primary">
+                  Giới thiệu
+                </a>
+              </li>
+              <li className="my-2">
+                <a href="/service" className="text-black hover:text-primary">
+                  Dịch vụ
+                </a>
+              </li>
+              <li className="my-2">
+                <a href="/product" className="text-black hover:text-primary">
+                  Sản phẩm
+                </a>
+              </li>
+              <li className="my-2">
+                <a href="/contact" className="text-black hover:text-primary">
+                  Liên hệ
+                </a>
+              </li>
+              <li className="my-2">
+                <a href="/cart" className="text-black hover:text-primary">
+                  Giỏ hàng
+                </a>
+              </li>
+              <li className="my-2">
+                <a href="/login" className="text-black hover:text-primary">
+                  Đăng nhập
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </nav>
+    </header>
+  );
 }
--- src/containers/Pages/Product/Product.js
+++ src/containers/Pages/Product/Product.js
@@ -1,101 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../../components/Header/Header';
 import Footer from '../../../components/Footer/Footer';
 
 export default function Products() {
-    return (
-        <div>
-            <Header />
-            <div className="my-24 mx-10 md:mx-10 lg:mx-24">
-                <div className="grid grid-cols-1 md:grid-cols-4">
-                    <div className="">
-                        <div className="text-3xl border-b-2 border-primary">Sản phẩm dịch vụ</div>
-                        <ul>
-                            <li className="text-xl my-5 "><a href="/service" className="hover:text-primary">Dịch vụ sức khỏe</a></li>
-                            <li className="text-xl my-5 "><a href="/service" className="hover:text-primary">Dịch vụ khác</a></li>
-                        </ul>
+  const [products, setProducts] = useState([]);
 
-                    </div>
-                    <div className="col-span-3">
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                            <article class="overflow-hidden rounded-lg shadow-lg m-10 col-span-1 md:col-span-1/3 ">
-                                <a href="/detail">
-                                    <img alt="..." class="block h-auto w-full" src="https://demo037059.web30s.vn/datafiles/32953/upload/images/product/efferalgan-500mg.jpg" />
-                                </a>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8001/dev/product');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Fetched data:', data);
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-                                <div className="flex flex-row p-2 md:p-4 my-5">
-                                    <div className="text-2xl basis-2/3">
-                                        Efferalgan 500Mg
-                                    </div>
-                                    <div className="text-2xl text-primary basis-1/3">
-                                        50.000đ
-                                    </div>
-                                </div>
+    fetchData();
+  }, []);
 
-                                <div className="text-center pb-5">
-                                    <button className="text-xl font-bold rounded-xl p-2 border-primary border-2 text-primary hover:bg-primary hover:text-white transition duration-500">+ Giỏ hàng</button>
-                                </div>
-                            </article>
+  return (
+    <div>
+      <Header />
+      <div className="my-24 mx-10 md:mx-10 lg:mx-24">
+        <div className="grid grid-cols-1 md:grid-cols-4">
+          <div className="">
+            <div className="text-3xl border-b-2 border-primary">Sản phẩm dịch vụ</div>
+            <ul>
+              <li className="text-xl my-5">
+                <a href="/service" className="hover:text-primary">
+                  Dịch vụ sức khỏe
+                </a>
+              </li>
+              <li className="text-xl my-5">
+                <a href="/service" className="hover:text-primary">
+                  Dịch vụ khác
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-span-3">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {products.map(product => (
+                <article key={product.id} className="overflow-hidden rounded-lg shadow-lg m-10 col-span-1 md:col-span-1/3">
+                  <a href="/detail">
+                    <img alt={product.name} className="block h-auto w-full" src={`http://localhost:8001/res/images/product/${product.id}.jpg`} />
+                  </a>
 
-                            <article class="overflow-hidden rounded-lg shadow-lg m-10 col-span-1 md:col-span-1/3 ">
-                                <a href="/detail">
-                                    <img alt="..." class="block h-auto w-full" src="https://demo037059.web30s.vn/datafiles/32953/upload/images/product/efferalgan-500mg.jpg" />
-                                </a>
+                  <div className="flex flex-row p-2 md:p-4 my-5">
+                    <div className="text-2xl basis-2/3">{product.name}</div>
+                    <div className="text-2xl text-primary basis-1/3">{`${product.price}đ`}</div>
+                  </div>
 
-                                <div className="flex flex-row p-2 md:p-4 my-5">
-                                    <div className="text-2xl basis-2/3">
-                                        Efferalgan 500Mg
-                                    </div>
-                                    <div className="text-2xl text-primary basis-1/3">
-                                        50.000đ
-                                    </div>
-                                </div>
-
-                                <div className="text-center pb-5">
-                                    <button className="text-xl font-bold rounded-xl p-2 border-primary border-2 text-primary hover:bg-primary hover:text-white transition duration-500">+ Giỏ hàng</button>
-                                </div>
-                            </article>
-                            <article class="overflow-hidden rounded-lg shadow-lg m-10 col-span-1 md:col-span-1/3 ">
-                                <a href="#">
-                                    <img alt="..." class="block h-auto w-full" src="https://demo037059.web30s.vn/datafiles/32953/upload/images/product/efferalgan-500mg.jpg" />
-                                </a>
-
-                                <div className="flex flex-row p-2 md:p-4 my-5">
-                                    <div className="text-2xl basis-2/3">
-                                        Efferalgan 500Mg
-                                    </div>
-                                    <div className="text-2xl text-primary basis-1/3">
-                                        50.000đ
-                                    </div>
-                                </div>
-
-                                <div className="text-center pb-5">
-                                    <button className="text-xl font-bold rounded-xl p-2 border-primary border-2 text-primary hover:bg-primary hover:text-white transition duration-500">+ Giỏ hàng</button>
-                                </div>
-                            </article>
-                            <article class="overflow-hidden rounded-lg shadow-lg m-10 col-span-1 md:col-span-1/3 ">
-                                <a href="#">
-                                    <img alt="..." class="block h-auto w-full" src="https://demo037059.web30s.vn/datafiles/32953/upload/images/product/efferalgan-500mg.jpg" />
-                                </a>
-
-                                <div className="flex flex-row p-2 md:p-4 my-5">
-                                    <div className="text-2xl basis-2/3">
-                                        Efferalgan 500Mg
-                                    </div>
-                                    <div className="text-2xl text-primary basis-1/3">
-                                        50.000đ
-                                    </div>
-                                </div>
-
-                                <div className="text-center pb-5">
-                                    <button className="text-xl font-bold rounded-xl p-2 border-primary border-2 text-primary hover:bg-primary hover:text-white transition duration-500">+ Giỏ hàng</button>
-                                </div>
-                            </article>
-                        </div>
-                    </div>
-                </div>
+                  <div className="text-center pb-5">
+                    <button className="text-xl font-bold rounded-xl p-2 border-primary border-2 text-primary hover:bg-primary hover:text-white transition duration-500">+ Giỏ hàng</button>
+                  </div>
+                </article>
+              ))}
             </div>
-            <Footer />
+          </div>
         </div>
-    )
+      </div>
+      <Footer />
+    </div>
+  );
 }
