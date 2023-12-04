99a3ccdbda964e944c0acad2b563a0fc4807ccc2
quangtri <quangtri3032001@gmail.com>
2023-12-04 22:30:10 +0700

    update product

--- src/containers/Pages/Product/Product.js
+++ src/containers/Pages/Product/Product.js
@@ -8,7 +8,7 @@ export default function Products() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('http://localhost:8001/dev/product');
+        const response = await fetch('http://localhost:8001/api/v1/business/products');
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
