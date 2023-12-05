15ac30e173832c2c2ecaa4e069a62dcab9ffdec3
htringuyen <tringuyenh75@gmail.com>
2023-12-05 09:05:12 +0700

    add auth-api

---
+++ src/auth-api/JWTClient.js
@@ -0,0 +1,187 @@
+// export JwtClient object
+
+
+export class JWTClient {
+    static SESSION_CREATED_AT =  '__jwtc__iat';
+    static SESSION_DURATION =  '__jwtc__duration';
+
+    #origin;
+    #loginPath;
+    #logoutPath;
+    #refreshPath;
+    #accessToken;
+
+    constructor(origin, loginPath, refreshPath, logoutPath) {
+        this.#origin = origin;
+        this.#loginPath = loginPath;
+        this.#refreshPath = refreshPath;
+        this.#logoutPath = logoutPath;
+        this.#accessToken = null;
+    }
+
+    async login(username, password) {
+        let success = false;
+
+        const url = this.#origin + this.#loginPath;
+
+        await fetch(url, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Authorization': 'Basic ' + btoa(username + ":" + password)
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.authorized) {
+                    success = true;
+                    this.#accessToken = data.accessToken;
+                    this.setCookie(this.createdAtKey(), Date.now(), {expires: new Date(data.hardDuration * 1000 + Date.now())});
+                    this.setCookie(this.durationKey(), data.hardDuration * 1000, {expires: new Date(data.hardDuration * 1000 + Date.now())});
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
+            .finally();
+
+        return success;
+    }
+
+    async logout() {
+        await this.fetch(this.#logoutPath, {
+                method: 'POST',
+                credentials: 'include'
+            })
+            .then(res => res.status === 200)
+            .then(success => success ? console.log("LOGOUT SUCCESSFUL...") : console.log("LOGOUT FAILED..."))
+
+        this.#accessToken = null;
+        this.setCookie(this.createdAtKey(), "", {expires: new Date(0)});
+        this.setCookie(this.durationKey(), "", {expires: new Date(0)});
+    }
+
+    async refreshAccessToken()  {
+        console.log("REFRESHING ACCESS TOKEN...");
+        let success = false;
+
+        const url = this.#origin + this.#refreshPath;
+
+        await fetch(url, {
+            method: 'POST',
+            credentials: 'include'
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("REFRESHED ACCESS TOKEN: " + "Fetching data...")
+                if (data.authorized) {
+                    success = true;
+                    this.#accessToken = data.accessToken;
+                    console.log("REFRESHED ACCESS TOKEN: Successfully");
+                }
+            })
+            .catch(err => {
+                console.log("REFRESHED ACCESS TOKEN: Failed");
+                console.log(err);
+            })
+            .finally();
+
+        return success;
+    }
+
+    async fetch(path, options = {}) {
+        let success = false;
+
+        const url = this.#origin + path;
+
+        // add Authorization header to options
+        options.headers = options.headers || {};
+
+        options.headers['Authorization'] = 'Bearer ' + this.#accessToken;
+
+        let response = await fetch(url, options);
+
+        if (response.status === 401) {
+            success = await this.refreshAccessToken();
+            if (success) {
+                options.headers['Authorization'] = 'Bearer ' + this.#accessToken;
+                response = await fetch(url, options);
+            }
+        }
+
+        return response;
+    }
+
+    async fetchJson(url, options = {}) {
+        return this.fetch(url, options).then(response => response.json());
+    }
+
+    stillHasTokenAfter(seconds) {
+        let createdAt = parseInt(this.getCookie(this.createdAtKey()));
+        let duration = parseInt(this.getCookie(this.durationKey()));
+
+        console.log("Checking local token// CREATED AT: " + createdAt + "; DURATION: " + duration +
+            "; NOW: " + Date.now() + "; REMAINING: " + (createdAt + duration - Date.now()));
+
+        if (createdAt && duration) {
+            return (createdAt + duration) > (Date.now() + seconds*1000);
+        }
+        return false;
+    }
+
+
+    getCookie(name) {
+        const cookies = document.cookie.split(';').map(cookie => cookie.trim());
+        for (const cookie of cookies) {
+            const [cookieName, cookieValue] = cookie.split('=');
+            if (cookieName === name) {
+                return cookieValue;
+            }
+        }
+        return null;
+    }
+
+    setCookie(name, value, options = {}) {
+        let cookieString = `${name}=${value}`;
+
+        if (options.expires) {
+            const expirationDate = new Date(options.expires).toUTCString();
+            cookieString += `; expires=${expirationDate}`;
+        }
+
+        if (options.path) {
+            cookieString += `; path=${options.path}`;
+        }
+
+        if (options.domain) {
+            cookieString += `; domain=${options.domain}`;
+        }
+
+        if (options.secure) {
+            cookieString += `; secure`;
+        }
+
+        if (options.httpOnly) {
+            cookieString += `; HttpOnly`;
+        }
+
+        document.cookie = cookieString;
+    }
+
+    createdAtKey() {
+        return JWTClient.SESSION_CREATED_AT + "_" + this.#origin;
+    }
+
+    durationKey() {
+        return JWTClient.SESSION_DURATION + "_" + this.#origin;
+    }
+}
+
+/************** EXPORT objects **************/
+export const jwtClient = new JWTClient(
+    "http://localhost:8001",
+    "/api/v1/common/auth/login",
+    "/api/v1/common/auth/refresh",
+    "/api/v1/common/auth/logout"
+    );
---
+++ src/auth-api/test.js
@@ -0,0 +1,39 @@
+import {jwtClient} from "./JWTClient.js";
+
+
+
+async function requestData() {
+    const response = await jwtClient.fetch("/api/v1/dev/products");
+    const data = await response.json();
+    console.log(data);
+}
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// login
+if (!jwtClient.stillHasTokenAfter(7200)) { // if token is not valid after 7200 seconds, redirect to login page
+    // redirect to login page
+    // because this is example, we will login here
+    if (await jwtClient.login("dr_john", "hashed_password")) {
+        console.log("Login successful...");
+    }
+    else {
+        console.log("Login failed...");
+    }
+    /***** BIG NOTE: In actually situation, redirect to login page. This is just example *****/
+}
+
+else {
+    console.log("Token is still valid... no need to login...");
+}
+
+// repeatedly request data
+for (let i = 0; i < 10; i++) {
+    await requestData();
+    await sleep(5000);
+}
+
+// logout
+await jwtClient.logout();
+
+
